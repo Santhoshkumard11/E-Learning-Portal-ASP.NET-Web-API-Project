b5d8e9c491917f7eabb177df0a5abe4e3a7c5ce8
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-03 13:59:10 +0530

    added employee and department models

---
+++ DAL/HandsOnWebAPIContext.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace HandsOnWebAPI.Models
+{
+    public class HandsOnWebAPIContext : DbContext
+    {
+        // Your context has been configured to use a 'Model1' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'HandsOnWebAPI.Models.Model1' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Model1' 
+        // connection string in the application configuration file.
+        public HandsOnWebAPIContext()
+            : base("name=HandsOnWebAPIContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HandsOnWebAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+
+        public string  LastName { get; set; }
+
+        public float Salary { get; set; }
+
+        //Foreign Key
+        public int DepartmentId { get; set; }
+        //Navigation Property
+        public Decimal Department { get; set; }
+    }
+}
