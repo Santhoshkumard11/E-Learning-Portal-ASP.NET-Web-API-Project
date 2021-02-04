e971141e591b12ff0e218e3c95864a51f8312eed
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-04 10:35:51 +0530

    added db connection and migration- seed

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -6,14 +6,14 @@ namespace HandsOnWebAPI.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<HandsOnWebAPI.Models.HandsOnWebAPIContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<HandsOnWebAPIContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(HandsOnWebAPI.Models.HandsOnWebAPIContext context)
+        protected override void Seed(HandsOnWebAPIContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -27,9 +27,18 @@ namespace HandsOnWebAPI.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Departments.AddOrUpdate(
+                new Department { Id = 100, Name = "HR" },
+                new Department { Id = 101, Name = "Technical" }
+
+                );
+
             context.Employees.AddOrUpdate(
             new Employee { Id = 1, FirstName = "Santhosh", LastName = "Kumar", DepartmentId = 101, Salary = 30000 },
-            new Employee { Id = 2, FirstName = "Sindhuja", LastName = "Kumar", DepartmentId = 100, Salary = 20000 }
+            new Employee { Id = 2, FirstName = "Sindhuja", LastName = "G", DepartmentId = 100, Salary = 20000 },
+            new Employee { Id = 3, FirstName = "Sankar", LastName = "Narayanan", DepartmentId = 101, Salary = 20000 },
+            new Employee { Id = 4, FirstName = "Steve", LastName = "Rogers", DepartmentId = 100, Salary = 20000 }
             );
         }
     }
