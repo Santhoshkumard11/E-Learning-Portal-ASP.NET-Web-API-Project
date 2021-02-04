21d5b79904b4289db83378539c3f9a8c992dfee4
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-04 23:32:10 +0530

    added test

---
+++ Tests/EmployeeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using HandsOnWebAPI.Controllers;
+using HandsOnWebAPI.Models;
+using NUnit.Framework;
+using System.Linq;
+
+namespace HandsOnWebAPI.Tests.Controllers
+{
+    [TestFixture]
+    public class EmployeeControllerTest
+    {
+        [Test]
+        public void BasicResultTesting()
+        {
+            EmployeesController controller = new EmployeesController();
+
+            IQueryable<Employee> result = controller.GetEmployees();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Santhosh", result.ToArray()[0]);
+            Assert.AreEqual("Santhosh", result.ElementAt(1));
+        }
+    }
+}
