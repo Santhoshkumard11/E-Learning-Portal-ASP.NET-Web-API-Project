dcff9eade66677750ff18923b762cb7ed5ae4a75
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-06 11:10:47 +0530

    doen T03-005

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -94,5 +94,61 @@ namespace HandsOnWebAPI.Controllers
             return Request.CreateResponse(HttpStatusCode.Created, student);
         }
 
+    
+        public HttpResponseMessage Delete(int id)
+        {
+
+            try
+            {
+                Student queriedStudent = db.Students.FirstOrDefault(s => s.Id == id);
+
+                if (queriedStudent == null)
+                {
+                   return GenerateResponseMessage(400, $"Student with Id {id} not found!! So not deleted!!");
+                }
+                else
+                {
+                    db.Students.Remove(queriedStudent);
+                    db.SaveChanges();
+                    return GenerateResponseMessage(202, $"Student with Id {id} deleted successfully!!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+
+        }
+    
+
+        //create a method which will return a response by taking in the status code and message
+        /// <summary>
+        /// Generate the response for the 
+        /// </summary>
+        /// <param name="statusCode">Response Status Code</param>
+        /// <param name="message"> Message as string or models</param>
+        /// <returns>Http response message</returns>
+        public HttpResponseMessage GenerateResponseMessage(int statusCode, object message)
+        {
+
+            var statusCodeType = new object() ;
+
+            switch (statusCode)
+            {
+                case  200:{statusCodeType = HttpStatusCode.OK;break; }
+                case  202:{statusCodeType = HttpStatusCode.Accepted;break; }
+                case  400:{statusCodeType = HttpStatusCode.BadRequest;break; }
+                
+                default:
+                    break;
+            }
+
+            return Request.CreateResponse((HttpStatusCode)statusCodeType, message);
+
+        }
+    
     }
 }
